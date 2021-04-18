f49143dbd7af3d55dd0dc90c27cdec8ffd91148a
Daniel <thanhqd@myumanitoba.ca>
2021-04-17 21:50:16 -0400

    fixed Remaining_Shortest_Job_First

--- coffee_task.c
+++ coffee_task.c
@@ -120,7 +120,6 @@ int compareTaskSJF(const task_t *first_task, const task_t *second_task) {
         if (firstTaskCpuTimeRemain < secondTaskCpuTimeRemain) {
             return 1;
         } /* end if */
-
         /* current running task will have higher position, so if equal happen
          * refer current runing task to continue running for not switch context */
         if (firstTaskCpuTimeRemain >= secondTaskCpuTimeRemain) {
@@ -132,6 +131,12 @@ int compareTaskSJF(const task_t *first_task, const task_t *second_task) {
     } else if (__isReadyToRun(second_task)) {
         /* only second task is ready so set it to higher position to be select */
         return -1;
+    }
+    else {
+        if (first_task->task_info->taskId < second_task->task_info->taskId) 
+        {
+            return 1;
+        } 
     }
 
     return 0;
